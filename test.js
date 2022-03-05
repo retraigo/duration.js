05b2d2cfdcaff3a4f92a5bf5b21890b71498078e
retraigo <nett@nekooftheabyss.xyz>
2022-03-05 20:15:53 +0530

    fix regex to not accept weird matches

--- test.js
+++ test.js
@@ -14,4 +14,4 @@ console.log(new Duration(114750).json);
 console.log(new Duration(245074).array);
 
 */
-console.log(Duration.fromString("4939  days 7342  hour 324milliseconds 4344 min").getFormattedDuration("s", "h"))
+ console.log(Duration.fromString("4min 54h 5d 44 s"))
